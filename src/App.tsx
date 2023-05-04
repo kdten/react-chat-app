4345ace8c17fd5b33bd24fddccdbef55f062fa40
kdten <kolten.rasmussen@gmail.com>
2023-05-04 16:20:16 -0600

    adds ts install

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useState } from 'react'
+
+
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth'
+
+
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollection } from 'react-firebase-hooks/firestore'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p>Hello world!</p>
+    </>
+  )
+}
+
+export default App
